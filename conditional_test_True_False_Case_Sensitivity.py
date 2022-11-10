66e46fb5062aa4cf41802ef146bab7405cfcb247
hap-py-python <117331957+hap-py-python@users.noreply.github.com>
2022-11-09 23:19:09 -0500

    Add files via upload

---
+++ conditional_test_True_False_Case_Sensitivity.py
@@ -0,0 +1,12 @@
+# conditional test
+
+fruit = 'apple'
+
+print("Is fruit == 'apple'? I predict True")
+print(fruit == 'apple')
+
+print("Is fruit == 'orange? I predict False")
+print(fruit == 'orange')
+
+print("Is fruit == 'Apple'? I predict False")
+print(fruit == 'Apple')
