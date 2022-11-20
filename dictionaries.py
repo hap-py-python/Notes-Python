31493ca4da898c7d10fb4dfe45a1279851c6b135
hap-py-python <117331957+hap-py-python@users.noreply.github.com>
2022-11-19 23:36:23 -0500

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+# Dictionaries
+
+milkshake_ingredients = {'fruits': 'strawberry, mango, banana', 'milk': 'oatmeal milk', 'toppings': 'cherry, chocolate'}
+
+print(milkshake_ingredients)
+print(milkshake_ingredients['fruits'])
+
